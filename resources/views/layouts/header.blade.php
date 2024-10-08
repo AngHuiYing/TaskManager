aec369e549cf89e2b6f78bc75c3b053d71696063
AngHuiYing <huiyingsyzz@gmail.com>
2024-10-08 13:31:30 +0800

    Adding layouts header and footer

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+
+        <nav class="navbar">
+            <ul>
+                <li><a href="">Task List</a></li>
+                <li><a href="">Add Task</a></li>
+            </ul>
+        </nav>
+ 
